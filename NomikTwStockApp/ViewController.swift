3d3b2b2542f545ada2350471d540e0904aa2529a
NomikLee <Nomiklee.icloud.com>
2024-05-19 12:40:00 +0800

    Initial Commit

---
+++ NomikTwStockApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NomikTwStockApp
+//
+//  Created by Pinocchio on 2024/5/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+
+
+}
+
